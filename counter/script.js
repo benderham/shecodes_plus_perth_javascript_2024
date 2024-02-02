a74f8f8f998a8ccdede021d9fd9b9de2a673bb5c
Ben Derham <1805685+benderham@users.noreply.github.com>
2024-02-02 20:14:02 +0800

    counter exercise complete

---
+++ counter/script.js
@@ -0,0 +1,18 @@
+function add(step) {
+  let numberElement = document.getElementById('number');
+  let number = numberElement.innerText;
+  number = parseInt(number) + parseInt(step);
+  numberElement.innerText = number;
+}
+
+function subtract(step) {
+  let numberElement = document.getElementById('number');
+  let number = numberElement.innerText;
+  number = parseInt(number) - parseInt(step);
+  numberElement.innerText = number;
+}
+
+function reset() {
+  let numberElement = document.getElementById('number');
+  numberElement.innerText = 0;
+}
